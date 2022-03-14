0170cfc91321b5c2258063d7b678bc6d3dfbd9c2
Felix Yeboah <gabsco208309@gmail.com>
2022-03-14 11:46:44 +0000

    new changes

--- src/lib/components/IconBox.tsx
+++ src/lib/components/IconBox.tsx
@@ -1,9 +1,21 @@
-import { Icon, Stack, Text } from "@chakra-ui/react";
+import { Icon, Image, Stack, Text } from "@chakra-ui/react";
 
-const IconBox = ({ icon, text }: { icon: any; text: string }) => {
+const IconBox = ({
+  icon,
+  text,
+  img,
+}: {
+  icon?: any;
+  text: string;
+  img?: string;
+}) => {
   return (
     <Stack textAlign="center" align="center">
-      <Icon as={icon} boxSize={16} />
+      {img ? (
+        <Image w={16} h={16} src={`/images/${img}.png`} alt={text} />
+      ) : (
+        <Icon as={icon} boxSize={16} />
+      )}
       <Text
         fontSize={{ md: "lg" }}
         css={{
--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,19 +1,20 @@
 import { Box, Heading, HStack, Stack } from "@chakra-ui/react";
+import { FiRefreshCcw } from "react-icons/fi";
 
 import IconBox from "../../components/IconBox";
 import AboutSection from "../../sections/AboutSection";
 import CtaSection from "../../sections/CTASection";
 import FaqSection from "../../sections/FAQSection";
 import HomeHeader from "../../sections/HomeHeader";
-import { Accessible, HandyTool, Professional, Speed } from "../../styles/icons";
+import { HandyTool, Speed } from "../../styles/icons";
 
 const Home = () => {
   const features = [
     { id: 1, icon: Speed, text: "Speed of Use" },
     { id: 2, icon: HandyTool, text: "Handy Tool" },
-    { id: 2, icon: Accessible, text: "Accessible Anytime" },
-    { id: 2, icon: HandyTool, text: "Responsive" },
-    { id: 2, icon: Professional, text: "Professional Service" },
+    { id: 2, img: "accessible", text: "Accessible Anytime" },
+    { id: 2, icon: FiRefreshCcw, text: "Responsive" },
+    { id: 2, img: "professional", text: "Professional Service" },
   ];
 
   return (
@@ -26,7 +27,12 @@ const Home = () => {
 
           <HStack align="center" justify="space-between">
             {features.map((item) => (
-              <IconBox key={item.id} icon={item.icon} text={item.text} />
+              <IconBox
+                key={item.id}
+                icon={item.icon}
+                text={item.text}
+                img={item.img}
+              />
             ))}
           </HStack>
         </Stack>
