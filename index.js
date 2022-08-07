5fd5d78dda49b2158b02c941350da4c515a3bbb6
David Arthur Assan <assan@Assans-MacBook-Pro.local>
2022-08-07 22:18:06 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index',{
+        title:"SHARP"
+    })
+  })
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "pug");
+
+// Server configuration
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+
+
+
+
